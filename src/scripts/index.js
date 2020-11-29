c568339407aa36f18a7403c651a410e7026d2a73
maksympohribniak <mpohribniak@emergemarket.com>
2020-11-29 16:25:08 +0200

    phone test

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,8 +1,52 @@
-import '../styles/index.scss';
-import '../scripts/shape-outside.js';
+import "../styles/index.scss";
+import "../scripts/shape-outside.js";
 
-document.addEventListener('DOMContentLoaded', () => {
-    objectFitImages(); // IE & Edge 12-15 don't support object-fit
+function addClass(ele, cls) {
+  if (ele.classList) {
+    ele.classList.add(cls);
+  } else if (!hasClass(ele, cls)) {
+    ele.setAttribute("class", ele.getAttribute("class") + " " + cls);
+  }
+}
 
-    window.ShapesPolyfill.run();
+function removeClass(ele, cls) {
+  if (ele.classList) {
+    ele.classList.remove(cls);
+  } else if (hasClass(ele, cls)) {
+    ele.setAttribute("class", ele.getAttribute("class").replace(cls, " "));
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  objectFitImages(); // IE & Edge 12-15 don't support object-fit
+
+  window.ShapesPolyfill.run(); // run polyfills IE
+
+  var open = document.getElementById("open-menu");
+  var close = document.getElementById("close-menu");
+  var backdrop = document.getElementById("backdrop");
+
+  var menu = document.getElementById("menu");
+
+  function closeMenu() {
+    removeClass(menu, "open");
+    window.document.body.removeAttribute("style");
+  }
+
+  function openMenu() {
+    addClass(menu, "open");
+    window.document.body.setAttribute("style", "overflow: hidden");    
+  }
+
+  open.onclick = openMenu;
+  backdrop.onclick = closeMenu;
+  close.onclick = closeMenu;
+
+  var elements = menu.querySelectorAll("a");
+
+  for (var i = 0; i < elements.length; i++) {
+    (function (element) {
+      element.addEventListener("click", closeMenu);
+    })(elements[i]);
+  }
 });
